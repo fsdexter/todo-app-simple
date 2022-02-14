e9490a2fd8c358d0b91e8620f754b85240971a20
fsdexter <fsdexter@gmail.com>
2022-02-14 11:43:40 +0100

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Guid } from "guid-typescript";
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app-simple';
+  todos: Todo[] = [
+    new Todo(Guid.create(),"learne TypeScript", false),
+    new Todo(Guid.create(),"learne Angular", false),
+  ];
+  onSubmit(form: NgForm){
+    let todo = new Todo(Guid.create(), form.value.newTask, false);
+    this.todos.push(todo);
+    form.resetForm();
+  }
+  onComplete(id: Guid){
+    let todo = this.todos.filter(todo => todo.id === id)[0];
+    todo.isCompleted = true;
+  }
+  onDelete(id: Guid){
+    let todo = this.todos.filter(todo => todo.id === id)[0];
+    let index = this.todos.indexOf(todo,0)
+    if(index > -1){
+      this.todos.splice(index,1)
+    }
+  }
 }
